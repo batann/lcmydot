b039f7e4fb3f5dae8c3d385d34b86fc10ef23c0e
batan <122584289+batann@users.noreply.github.com>
2025-08-04 20:12:22 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+if [[ -d /home/batan/lcmydot/images ]]; then
+rm -r /home/batan/lcmydot/images
+fi
 for X in $(ls /home/batan/lcmydot/*); do
-	if [[ -f /home/batan/.$X ]]; then
-		mv /home/batan/.$X /home/batan/.$X.org
+	if [[ -f /home/batan/.$(echo $X|rev|sed 's![/].*$!!g'|rev) ]]; then
+		mv /home/batan/.$(echo $X|rev|sed 's![/].*$!!g'|rev) /home/batan/.$(echo $X|rev|sed 's![/].*$!!g'|rev).org
 	fi
-	cp /home/batan/lcmydot/$X /home/batan/.$X
+	cp $X /home/batan/.$(echo $X|rev|sed 's![/].*$!!g'|rev)
 done
 
