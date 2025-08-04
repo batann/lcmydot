d7aa117226d395ef748ca36fce714a812cf2e14d
batan <122584289+batann@users.noreply.github.com>
2025-08-04 20:13:09 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [[ -d /home/batan/lcmydot/images ]]; then
-rm -r /home/batan/lcmydot/images
+sudo rm -r /home/batan/lcmydot/images
 fi
 for X in $(ls /home/batan/lcmydot/*); do
 	if [[ -f /home/batan/.$(echo $X|rev|sed 's![/].*$!!g'|rev) ]]; then
